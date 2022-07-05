a5813310d75ceefb6fea4809c0bf4efa3d8162cc
Vladyslav Labenskiy <vladiklabensky@gmail.com>
2022-07-05 19:43:21 +0300

    Added animation

--- client/src/components/TodoItem/TodoItem.tsx
+++ client/src/components/TodoItem/TodoItem.tsx
@@ -6,13 +6,13 @@ interface ITodoProps {
 
 export const TodoItem = ({ todo }: ITodoProps) => {
   return (
-    <li>
+    <li className="list-group-item d-flex justify-content-between">
       <div className="">
         <span>{todo.title}</span>
       </div>
       <div>
-        <button className="btn btn-primary">Update</button>
-        <button className="btn btn-success">Complete</button>
+        <button className="btn btn-primary me-2">Update</button>
+        <button className="btn btn-success me-2">Complete</button>
         <button className="btn btn-danger">Delete</button>
       </div>
     </li>
--- client/src/components/TodoList/TodoList.tsx
+++ client/src/components/TodoList/TodoList.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { TodoItem } from "../TodoItem/TodoItem";
 import { ITodoReducer } from "../types/types";
+import './style.css'
 
 export const TodoList = () => {
   const state = useSelector((state: ITodoReducer) => state.todoReducer);
@@ -9,12 +10,8 @@ export const TodoList = () => {
   return (
     <TransitionGroup component="ul" className="list-group">
       {state.todos.map((todo) => (
-        <CSSTransition
-          timeout={700}
-          classNames={"post"}
-          key={todo.id}
-        >
-            <TodoItem todo={todo}/>
+        <CSSTransition timeout={700} classNames={"todo"} key={todo.id}>
+          <TodoItem todo={todo} />
         </CSSTransition>
       ))}
     </TransitionGroup>
